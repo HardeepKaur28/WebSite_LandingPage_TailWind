016ef60a12467918f9d2df7a6d55a2578dd5669a
HardeepKaur <hardeepkaur2768@gmail.com>
2024-07-31 12:31:36 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base :"/WebSite_LandingPage_TailWind/",
   plugins: [react()],
-  base: "/WebSite_LandingPage_TailWind",
 })
